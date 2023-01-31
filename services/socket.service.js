9d913f539e92440c57fa3407f6617e7657c315a0
Shaked Barsheshet <alive2154@gmail.com>
2023-01-31 14:08:58 +0200

    naknikia

--- services/socket.service.js
+++ services/socket.service.js
@@ -9,6 +9,7 @@ function setupSocketAPI(http) {
         }
     })
     gIo.on('connection', socket => {
+        console.log('log', socket)
         socket.on('disconnect', socket => {
         })
         socket.on('chat-register-users-to-channel', topic => {
